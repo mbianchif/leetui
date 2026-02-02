c1b9d4538b0855c2136a4fd2c98be71c5625798c
mbianchif <89595589+mbianchif@users.noreply.github.com>
2026-02-01 23:23:57 -0300

    feat: main upgrade

--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,16 +1,9 @@
 mod app;
 
-use std::{env, error::Error, io, time::Duration};
+use std::{env, error::Error, time::Duration};
 
 use api::LeetCodeClient;
-use ratatui::{
-    Terminal,
-    backend::CrosstermBackend,
-    crossterm::{
-        execute,
-        terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
-    },
-};
+use ratatui::{Terminal, prelude::Backend};
 use tokio::{sync::mpsc, time};
 
 use app::{App, Zellij, handler};
@@ -25,27 +18,26 @@ fn retrieve_leetcode_vars() -> Result<(String, String), &'static str> {
     Ok((session, csrf))
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let (session, csrf) = retrieve_leetcode_vars()?;
-
+/// Setups and runs the entire application.
+///
+/// # Arguments
+/// * `terminal` - The terminal instance to use.
+///
+/// # Returns
+/// Any generic error the application has.
+async fn run_app<B: Backend + 'static>(terminal: &mut Terminal<B>) -> Result<(), Box<dyn Error>> {
     let (client_tx, client_rx) = mpsc::channel(100);
     let (action_tx, mut action_rx) = mpsc::channel(100);
     let throbber_interval = time::interval(Duration::from_millis(30));
+
+    let (session, csrf) = retrieve_leetcode_vars()?;
     let client = LeetCodeClient::new(session.clone(), csrf.clone())?;
+    let mut app = App::new(client_tx, Zellij);
 
     // Initialize the input listeners.
     tokio::spawn(handler::spawn_keyboard(action_tx.clone()));
     tokio::spawn(handler::spawn_ticker(action_tx.clone(), throbber_interval));
     tokio::spawn(handler::spawn_client(action_tx, client_rx, client));
-
-    // Setup the terminal backend.
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    let mut app = App::new(client_tx, Zellij);
 
     loop {
         terminal.draw(|f| app.render(f))?;
@@ -56,8 +48,13 @@ async fn main() -> Result<(), Box<dyn Error>> {
         }
     }
 
-    // Cleanup.
-    disable_raw_mode()?;
-    execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
     Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut terminal = ratatui::init();
+    let res = run_app(&mut terminal).await;
+    ratatui::restore();
+    res
 }
